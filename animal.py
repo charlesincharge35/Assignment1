63d5cc9e79dd623e70459e6aa1a438455cdb3708
Charles Daniel <daniecha@oregonstate.edu>
2021-06-23 20:09:08 -0500

    First python project submission.

---
+++ animal.py
@@ -0,0 +1,6 @@
+# Author: Charles Daniel
+# Date: 6/23/2021
+# Description: Asks the user for their favorite and then prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the "+fave_animal+"")
